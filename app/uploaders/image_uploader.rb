1e2d5520e5f8e9c75daa1b818a132d29ead9f08c
ines bahri <ines.bahri@yahoo.com>
2015-03-28 16:00:05 -0800

    Add sign in link

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,13 +12,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
 #   storage :fog
   
-
+def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
 
 # Process files as they are uploaded:
   # process :resize_to_fit => [300, 300]
